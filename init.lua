2f4db87d57703f4a84e8752616bc26c7f4814694
jdhubb99 <jacobdhubbard@gmail.com>
2023-04-08 12:07:08 -0400

    Add colorscheme and basic plugins

---
+++ init.lua
@@ -0,0 +1,8 @@
+require("jake.plugins-setup")
+require("jake.core.options")
+require("jake.core.keymaps")
+require("jake.core.colorscheme")
+require("jake.plugins.comment")
+require("jake.plugins.nvim-tree")
+require("jake.plugins.lualine")
+require("jake.plugins.telescope")
---
+++ lua/jake/plugins-setup.lua
@@ -0,0 +1,65 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+-- Autocommand that reloads neovim whenever this file is saved
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins-setup.lua source <afile> | PackerSync 
+  augroup end
+]])
+
+local status, packer = pcall(require, "packer")
+if not status then
+  return
+end
+
+return packer.startup(function(use)
+  use("wbthomason/packer.nvim")
+
+  -- lua functions that other plugins use
+  use("nvim-lua/plenary.nvim")
+
+  use("bluz71/vim-nightfly-guicolors") -- preferred colorscheme
+
+  -- tmux and split window navigation
+  use("christoomey/vim-tmux-navigator")
+
+  -- maxes and restores current window
+  use("szw/vim-maximizer")
+
+  -- essential plugins for workflow
+  use("tpope/vim-surround")
+  use("vim-scripts/ReplaceWithRegister")
+
+  -- comments with gc
+  use("numToStr/Comment.nvim")
+
+  -- file explorer
+  use("nvim-tree/nvim-tree.lua")
+
+  -- icons
+  use("kyazdani42/nvim-web-devicons")
+
+  -- status line
+  use("nvim-lualine/lualine.nvim")
+
+  -- telescope fuzzy finder
+  use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
+  use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
+  use({ "nvim-telescope/telescope-ui-select.nvim" }) -- for showing lsp code actions
+
+  if packer_bootstrap then
+    require("packer").sync()
+  end
+end)
